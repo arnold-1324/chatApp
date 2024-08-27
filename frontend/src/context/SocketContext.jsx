fd309709b0d22eb1e8be117b9ab06406770b851e
arnold-1324 <anlsktl1318@gmail.com>
2024-08-27 23:18:50 +0530

    socketIO

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://chatapp-7r24.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
