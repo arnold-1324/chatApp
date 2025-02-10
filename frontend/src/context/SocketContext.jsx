8fde0b235fe09a0795318347497f01cbc4cdcd0c
Anold <141809292+arnold-1324@users.noreply.github.com>
2025-02-10 11:04:52 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chatapp-production-4bbc.up.railway.app", {
 				query: {
 					userId: authUser._id,
 				},
